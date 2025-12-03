38f42da54d50578793c0c46d8b0efe271bd434ff
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-03 13:05:22 -0800

    Create route for post look up by slug

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -2,5 +2,6 @@ const postsRouter = require("express").Router();
 const postsController = require("../controllers/postsController");
 
 postsRouter.get("/", postsController.getAllPosts);
+postsRouter.get("/:slug", postsController.getPostBySlug);
 
 module.exports = postsRouter;
