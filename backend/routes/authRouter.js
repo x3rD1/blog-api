81aa40840a5de9bb3f1ef80e2075f38dc624c438
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-03 02:26:39 -0800

    Applied validation and handleValidation

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,10 +1,18 @@
 const { Router } = require("express");
 const authRouter = Router();
 const authController = require("../controllers/authController");
+const { validateSignup } = require("../validators/signupValidator");
+const { handleValidation } = require("../middlewares/handleValidation");
 
 authRouter.get("/login", authController.getLoginPage);
 authRouter.post("/login", authController.login);
 
 authRouter.get("/signup", authController.getSignupPage);
-authRouter.post("/signup", authController.signup);
+authRouter.post(
+  "/signup",
+  validateSignup,
+  handleValidation,
+  authController.signup
+);
+
 module.exports = authRouter;
