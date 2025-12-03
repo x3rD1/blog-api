860556193e77c08793f1c6c0fa28a087747dcac5
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-02 17:55:05 -0800

    Add express.json and express.urlencoded

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Welcome to blog api");
 });
