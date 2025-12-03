47de2531a2392a0d1ed2238e3307a2380544ca91
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-03 13:04:29 -0800

    Create slugify function for slug parameter

---
+++ backend/utilities/slugify.js
@@ -0,0 +1,9 @@
+exports.slugify = (text) => {
+  return text
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9 -]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-")
+    .replace(/^-+|-+$/g, "");
+};
