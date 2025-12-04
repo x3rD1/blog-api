bf6ceab7ce8d94bd2aedbdde232648e7e699cf9d
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-03 20:08:17 -0800

    Add isAdmin and authenticateToken middlware

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -2,12 +2,15 @@ const postsRouter = require("express").Router();
 const postsController = require("../controllers/postsController");
 const { validatePost } = require("../validators/postValidator");
 const { handleValidation } = require("../middlewares/handleValidation");
+const { isAdmin } = require("../middlewares/isAdmin");
+const { authenticateToken } = require("../middlewares/authenticateToken");
 
-postsRouter.get("/", postsController.getAllPosts);
-postsRouter.get("/:slug", postsController.getPostBySlug);
+postsRouter.get("/", authenticateToken, postsController.getAllPosts);
+postsRouter.get("/:slug", authenticateToken, postsController.getPostBySlug);
 
 postsRouter.post(
   "/:slug",
+  isAdmin,
   validatePost,
   handleValidation,
   postsController.createPost
