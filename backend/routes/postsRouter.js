757d0051db87e5dcb7c3b293d78749ea5f181c43
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-03 19:01:40 -0800

    Add route for creating a post + validation

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -1,7 +1,16 @@
 const postsRouter = require("express").Router();
 const postsController = require("../controllers/postsController");
+const { validatePost } = require("../validators/postValidator");
+const { handleValidation } = require("../middlewares/handleValidation");
 
 postsRouter.get("/", postsController.getAllPosts);
 postsRouter.get("/:slug", postsController.getPostBySlug);
+
+postsRouter.post(
+  "/:slug",
+  validatePost,
+  handleValidation,
+  postsController.createPost
+);
 
 module.exports = postsRouter;
