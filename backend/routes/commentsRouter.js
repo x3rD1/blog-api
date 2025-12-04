b0dd2f2f25b0efe876bad6e436308c090b68958d
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-04 13:18:23 -0800

    Add route for comment creation

--- backend/routes/commentsRouter.js
+++ backend/routes/commentsRouter.js
@@ -1,7 +1,10 @@
 const commentsRouter = require("express").Router({ mergeParams: true });
 const commentsController = require("../controllers/commentsController");
+const { authenticateToken } = require("../middlewares/authenticateToken");
 
 commentsRouter.get("/", commentsController.getAllComments);
 commentsRouter.get("/:id", commentsController.getCommentById);
+
+commentsRouter.post("/", authenticateToken, commentsController.createComment);
 
 module.exports = commentsRouter;
