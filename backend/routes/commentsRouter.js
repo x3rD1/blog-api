3f93c987f1470b5cf18e60f8340633e976653316
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-04 12:32:24 -0800

    Add route for getting a comment by id

--- backend/routes/commentsRouter.js
+++ backend/routes/commentsRouter.js
@@ -2,5 +2,6 @@ const commentsRouter = require("express").Router({ mergeParams: true });
 const commentsController = require("../controllers/commentsController");
 
 commentsRouter.get("/", commentsController.getAllComments);
+commentsRouter.get("/:id", commentsController.getCommentById);
 
 module.exports = commentsRouter;
