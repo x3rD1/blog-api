8dd969e0eb575d4df9a013e5380f8de19f8086c5
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-05 02:43:00 -0800

    Add routes for logout and token refresh

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -3,9 +3,12 @@ const authRouter = Router();
 const authController = require("../controllers/authController");
 const { validateSignup } = require("../validators/signupValidator");
 const { handleValidation } = require("../middlewares/handleValidation");
+const { authenticateToken } = require("../middlewares/authenticateToken");
 
 authRouter.get("/login", authController.getLoginPage);
 authRouter.post("/login", authController.login);
+authRouter.post("/logout", authenticateToken, authController.logout);
+authRouter.post("/token/refresh", authController.refreshToken);
 
 authRouter.get("/signup", authController.getSignupPage);
 authRouter.post(
