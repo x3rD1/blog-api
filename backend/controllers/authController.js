e2383a363b743ff5597cbb315ad2c348f1ea8788
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-05 02:42:19 -0800

    Create refreshToken function and implement it into login function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
+const {
+  createAccessToken,
+  createRefreshToken,
+} = require("../utilities/createToken");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const prisma = require("../config/prisma");
@@ -29,8 +33,12 @@ exports.login = async (req, res) => {
       admin: user.admin,
     };
 
-    const accessToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
-      expiresIn: "15m",
+    const accessToken = createAccessToken(payload);
+    const refreshToken = createRefreshToken(payload.sub);
+
+    res.cookie("jid", refreshToken, {
+      httpOnly: true,
+      path: "/auth/token/refresh",
     });
 
     res.json({ message: "Authenticated", accessToken, payload });
@@ -70,4 +78,46 @@ exports.signup = async (req, res) => {
     console.log(err);
     res.status(500).json({ message: "Something went wrong." });
   }
+};
+
+exports.refreshToken = async (req, res) => {
+  const token = req.cookies.jid;
+
+  if (!token)
+    return res
+      .status(401)
+      .json({ success: false, message: "No refresh token." });
+
+  let payload = null;
+  try {
+    payload = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+  } catch (err) {
+    console.log(err);
+    res.status(403).json({ success: false, message: "Invalid refresh token." });
+  }
+
+  const user = await prisma.user.findUnique({ where: { id: payload.sub } });
+  if (!user)
+    return res.status(404).json({ success: false, message: "User not found." });
+
+  const userPayload = {
+    sub: user.id,
+    email: user.email,
+    admin: user.admin,
+  };
+
+  const accessToken = createAccessToken(userPayload);
+  const refreshToken = createRefreshToken(userPayload.sub);
+
+  res.cookie("jid", refreshToken, {
+    httpOnly: true,
+    path: "/auth/token/refresh",
+  });
+
+  res.json({ success: true, accessToken });
+};
+
+exports.logout = (req, res) => {
+  res.clearCookie("jid", { path: "/auth/token/refresh" });
+  res.json({ success: true, message: "Logged out successfully!" });
 };
