6451e781144735ccfb70cf3eb311e7fb987bb637
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-05 21:26:45 -0800

    Add proxy for fetching api from server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
