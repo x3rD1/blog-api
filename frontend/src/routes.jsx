7a8e4b5762ecb1ed3eaf739d65d9caf218b65e94
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-06 18:26:39 -0800

    Add route for post/:slug path

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,6 +2,7 @@ import App from "./App";
 import ErrorPage from "./components/ErrorPage";
 import IndexPage from "./components/IndexPage";
 import BlogsPage from "./components/BlogsPage";
+import Post from "./components/Post";
 
 const routes = [
   {
@@ -11,6 +12,7 @@ const routes = [
     children: [
       { index: true, element: <IndexPage /> },
       { path: "blogs", element: <BlogsPage /> },
+      { path: "post/:slug", element: <Post /> },
     ],
   },
 ];
