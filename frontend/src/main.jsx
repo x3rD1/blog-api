91b4fbc2466c86a91875e8f7d9990fd67cc8fd78
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-10 15:13:19 -0800

    Add AuthProvider and wrap RouterProvider inside for child components to access global functions

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { AuthProvider } from "./context/AuthProvider";
 import routes from "./routes";
 import "./index.css";
 
@@ -8,6 +9,8 @@ const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
