8310caa80cdd3a7dca9daab7a5e258435e6916a6
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-11 19:44:21 -0800

    Create simple login feature for admin

---
+++ admin/src/Login.jsx
@@ -0,0 +1,65 @@
+import { useState, useContext } from "react";
+import AuthContext from "./context/authContext";
+import { useNavigate } from "react-router";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { setAccessToken } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.message);
+      }
+
+      if (!data.payload.admin) {
+        throw new Error("Unauthorized Access!");
+      }
+
+      setAccessToken(data.accessToken);
+      navigate("/");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h1>Admin Login</h1>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="Submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
