db8a7a41ecbb2993a591ab98d59949376e67eb66
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-12-11 19:46:07 -0800

    Add router provider and auth provider

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import routes from "./routes.jsx";
+import AuthProvider from "./context/AuthProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
